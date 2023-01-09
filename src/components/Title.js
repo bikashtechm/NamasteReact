444cb68c8c831ad6749e3fe9b30f43b99c1bbded
Bikash Shaw <1985.bikash.shaw@gmail.com>
2023-01-09 18:27:50 +0530

    latest

---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+const Title = () => {
+  return (
+    <a href="/">
+      <img
+        alt="logo"
+        className="logo"
+        src="https://static.vecteezy.com/system/resources/previews/006/395/395/original/food-point-logo-symbol-and-icon-template-to-show-the-location-of-the-food-seller-vector.jpg"
+      />
+    </a>
+  );
+};
+export default Title;
