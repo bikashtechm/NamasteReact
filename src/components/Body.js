c853dde7dd843bc1fb02c183027c03449195a8a3
Bikash Shaw <1985.bikash.shaw@gmail.com>
2023-01-09 23:50:09 +0530

    search button

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,11 +2,26 @@ import { RestaurantLists } from "../Constants";
 import RestaurantCard from "./RestaurantCard";
 const Body = () => {
   return (
-    <div className="restaurant-lists">
-      {RestaurantLists.map((restaurant) => {
-        return <RestaurantCard {...restaurant.data} key={restaurant.data.id} />;
-      })}
-    </div>
+    <>
+      <div class="search">
+        <input
+          type="text"
+          class="searchTerm"
+          placeholder="What are you looking for?"
+          value=""
+        />
+        <button type="submit" class="searchButton">
+          Search
+        </button>
+      </div>
+      <div className="restaurant-lists">
+        {RestaurantLists.map((restaurant) => {
+          return (
+            <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
+          );
+        })}
+      </div>
+    </>
   );
 };
 
