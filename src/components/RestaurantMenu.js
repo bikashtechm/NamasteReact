4b6a50f080c88f4bc5f04c337cb6277e72098bfa
Bikash Shaw <1985.bikash.shaw@gmail.com>
2023-01-25 16:44:34 +0530

    custome Hook for useRestaurant created

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,48 +1,36 @@
-import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { IMG_CDN_URL } from "../Constants";
-import AVARAT_IMG from "../assets/images/img_avatar.png";
 import Shimmer from "./Shimmer";
-import { FETCH_MENU_URL } from "../Constants";
+import useRestaurant from "../utils/useRestaurant";
 const RestaurantMenu = () => {
   const { resId } = useParams();
-  const [resInfo, setResInfo] = useState(null);
 
-  useEffect(() => {
-    getRestaurantInfo();
-  }, []);
+  const restaurant = useRestaurant(resId);
 
-  async function getRestaurantInfo() {
-    const data = await fetch(FETCH_MENU_URL + resId);
-    const json = await data.json();
-    setResInfo(json.data);
-    console.log(resInfo);
-  }
-
-  return !resInfo ? (
+  return !restaurant ? (
     <Shimmer />
   ) : (
     <div className="menuCard">
       <span className="restaurantcard">
         <img
           className="avatar"
-          src={IMG_CDN_URL + resInfo?.cloudinaryImageId}
-          alt={resInfo.name}
+          src={IMG_CDN_URL + restaurant?.cloudinaryImageId}
+          alt={restaurant.name}
         />
         <div className="restaurantcontainer">
           <h4>
-            <b>{resInfo.name}</b>
+            <b>{restaurant?.name}</b>
           </h4>
           <p>Restaurant ID : {resId}</p>
-          <p>Area : {resInfo.area}</p>
-          <p>City : {resInfo.city}</p>
-          <p>Rating : {resInfo?.avgRating} Stars</p>
+          <p>Area : {restaurant?.area}</p>
+          <p>City : {restaurant?.city}</p>
+          <p>Rating : {restaurant?.avgRating} Stars</p>
         </div>
       </span>
       <span className="menuList">
         <h1>Menu</h1>
         <ul>
-          {Object.values(resInfo?.menu?.items).map((item) => (
+          {Object.values(restaurant?.menu?.items).map((item) => (
             <li key={item.id}>{item.name}</li>
           ))}
         </ul>
